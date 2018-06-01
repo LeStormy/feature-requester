ac6d60742294dec281d6dd7bb9466904234877ac
Victor Goya <v.goya@millimages.com>
2018-06-01 17:00:01 +0200

    update some stuff

--- app/controllers/road_map_controller.rb
+++ app/controllers/road_map_controller.rb
@@ -1,21 +1,16 @@
 class RoadMapController < ApplicationController
   before_action :set_current_user
-  def show
 
+  def show
   end
 
   def update
-
   end
 
   def index
-    @boards = Hash[Board.all.pluck(:id, :title)]
-    @planned = Post.where(status: :planned)
-    @in_progress = Post.where(status: :in_progress)
-    @completed = Post.where(status: :completed)
+    @boards = Board.all
   end
 
   def create
-
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,27 @@
 module ApplicationHelper
+  def state_icon(state)
+    case state
+    when :planned
+      "sticky-note"
+    when :in_progress
+      "wrench"
+    when :completed
+      "check"
+    else
+      "exclamation-circle"
+    end
+  end
+
+  def state_color(state)
+    case state
+    when :planned
+      "#1fa0ff"
+    when :in_progress
+      "#c17aff"
+    when :completed
+      "#6dd345"
+    else
+      "red"
+    end
+  end
 end
